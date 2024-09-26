afad483979eac5ea76abca5e54a8e8db8f440831
salomekhel <salomekhelashvili2023@u.northwestern.edu>
2024-09-26 00:14:08 -0500

    birthday posts

--- routes/spotifyRoutes.js
+++ routes/spotifyRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const spotifyController = require('../controllers/spotifyController');
+const path = require('path');
 
 // Define your routes here
 router.get('/login', spotifyController.spotifyLogin);
@@ -10,5 +11,9 @@ router.get('/refresh-token', spotifyController.refreshAccessToken);
 // Route for top artist (add this)
 router.get('/user-top-artist', spotifyController.getUserTopArtist);  // Ensure this is correctly pointing to the controller
 
+// Your existing routes here
+router.get('/post-birthday-message', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'post-birthday-message.html'));
+});
 
 module.exports = router;
